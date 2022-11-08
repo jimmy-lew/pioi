7c783a1af7931f1d9a92f2b7125a4e3a4b79edd4
jimmy <s10219505@connect.np.edu.sg>
2022-11-09 07:17:07 +0800

    temp: add theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,31 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+			
+					"primary": "#d6d1cb",
+								
+					"secondary": "#FF6600",
+								
+					"accent": "#09BB6F",
+								
+					"neutral": "#ede8e4",
+								
+					"base-100": "#222",
+								
+					"info": "#0ea5e9",
+								
+					"success": "#22c55e",
+								
+					"warning": "#f59e0b",
+								
+					"error": "#be123c",
+				},
+			},
+		],
+	},
 	plugins: [require('daisyui')],
 }
