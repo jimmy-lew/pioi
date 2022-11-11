3c8db071ef8d7818a4da40ae766fa31ce4eafe12
jimmy <s10219505@connect.np.edu.sg>
2022-11-11 12:58:28 +0800

    feat: update app store

--- stores/index.ts
+++ stores/index.ts
@@ -1,7 +1,10 @@
 import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', () => {
-	const elementInViewIndex = ref(0)
+	const elementInView = ref(0)
+	function setElementInView(index: number) {
+		elementInView.value = index
+	}
 
-	return { elementInViewIndex }
+	return { elementInView, setElementInView }
 })
